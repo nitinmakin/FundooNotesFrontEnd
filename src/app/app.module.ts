479f778a4bd40c68675926bfbe439d4a601a0a88
nitinmakin <nitinmakin124.com>
2020-11-03 21:14:22 +0530

    [Nitin Makin] Added code for Register UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+// import {Component} from '@angular/core';
+// import {FormControl, Validators} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule
+    // Component,
+    // FormControl,
+    // Validators
   ],
   providers: [],
   bootstrap: [AppComponent]
